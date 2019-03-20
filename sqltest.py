779a557c9c3a6b226bac36f04eb692f362082465
Junichi Kawahara <kawahara-j@hi-ad.jp>
2019-03-20 13:32:07 +0900

    sqltest.py

---
+++ sqltest.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+
+CREATE_TABLE = '''CREATE TABLE books
+(id INTEGER PRIMARY KEY AUTOINCREMENT,
+author TEXT,
+title TEXT)'''
+
+TEST_INSERT = '''INSERT INTO books (title, author)
+VALUES ('プリンキピア', 'ニュートン')
+'''
+
+TEST_SELECT = '''SELECT * FROM books'''
+
+conn = sqlite3.connect('testdb.sqlite3')
+c = conn.cursor()
+c.execute(CREATE_TABLE)
+c.execute(TEST_INSERT)
+conn.commit()
+
+c.execute(TEST_SELECT)
+result = c.fetchone()
+print(result)
+conn.close()
